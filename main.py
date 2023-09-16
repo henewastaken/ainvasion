520b6b80468b9d3f7dc805d1e3ccd1f2554cf307
Petra <henry.andersson@tuni.fi>
2023-09-16 22:12:10 +0300

    Started working on json parsing from chatgpt text. ATM broken. Pushing so can pull to my laptop

--- main.py
+++ main.py
@@ -1,83 +1,124 @@
 import json
+import re
+import openai
 import random
 from countries import finland, sweden, norway, belgium, czech_republic, denmark, germany, netherlands, poland, countries
 from events import leader_effects
 from game_events import to_send, to_receive
 
+openai.api_key = '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'
+
 
 def main_loop():
 
+    STARTING_PROMPT = '''
+        From now on, you are a dungeon master in an imaginary turn base world conquer game. 
+        You will get a JSON containing data and events and you are required to come up with a short story about the event, the story can be as imaginative as you decide. 
+        You must also return a JSON data about the event. 
+        Also you are required to make decision about was the move a success or not.
+        Also you can give players random special items, but they have to fit in the story. Also give these items a names and an effects.
+        The events in the story must contain information from the JSON you receive and the JSON you return must also have data from your story.
+        The content of the JSON is as follows'''
 
-    STARTING_PROMPT =  '''
-        From now on, you are a dungeon master in an imaginary turn base world conquer game. You will get a JSON containing
-        data and events for moves and you are required to come up with a short story about the event, the story can be as imaginative as you deside. 
-        You must also return a JOSN data about the event. 
-        Also you are required to make desition about was the move a success or not.
-        Also you can give players random special items, but they have to fit in the story. Also give these items a name and an effect.
-        The events in the story must contain information from the JSON you recieve and the JSON you return must also have data from your story.
-        The content of the JSON will be described below.'''
-    
     ATTACK_JSON = '''
-        Attacking event. Player is trying to invade a counrty mentioned in the JSON with a military invasion:
+        Attacking event. Player is trying to invade a country mentioned in the JSON with a military invasion:
         to_attack = {
-            "in_turn": this field will contain the players name who is makin a move
-            "where": contains the country player are tryign to target.
-            "leader_Status": leader status will be type {"status": which tells the leader status, "effect": {effect name: effect}. Use the leader status in your story and decition making. Take in count the effect the leader has, but it's more a guideline, so in the end you make the desition, how much it affects the result.
-            "friendly_army_size": is numer of the players army size
+            "in_turn": this field will contain the players name who is making the move
+            "where": contains the country player are trying conquer.
+            "leader_Status": leader status will be type {"status": <which tells the leader status>, "effect": {<effect name>: <effect>}. You can Use the leader status in your story and decision making, but it is optional.
+            "friendly_army_size": is number of the players army size
             "friendly_army_status": is type of {"status": which tells the army status, "effect": {effect name: effect}. Work similarly to leader_status.
             "enemy_army_size": is number of enemy army (the country player are attacking)
             "enemy_army_status": is type of {"status": which tells the enemy army status, "effect": {effect name: effect}. Work similarly to leader_status.
             "enemy_leader_status": is type of {"status": which tells the enemy leader status, "effect": {effect name: effect}. Work similarly to leader_status.
             "used_special_resources": is type {"item": <item name>, "effect": <effect>} works similarly to leader_status
         }
-        From the JOSN data, you need to make a conclusion was the attack succesfull and give a short story of the events of the battle.
-        You and make the story as imaginative as possible. 
-        Use the army sizes, the different statused and their effects and the used_special_recources and their statuses. For example, friedly_army_size is 500 000 soldies larger than the enemy_army_size, but friendly_army_status is {"status": "Drunk", "effect": {"combat effectiveness": "decrease"}}, so take stuff like that into account, but you choose how much you will follow the data in the JSON.
+        From the JSON data, you need to make a conclusion was the attack successful.
+        Use the army sizes, the different statuses and their effects and the used_special_recourses and their statuses. 
+        For example, friendly_army_size is 500 000 soldiers larger than the enemy_army_size, but friendly_army_status is {"status": "Drunk", "effect": {"combat effectiveness": "decrease"}}, so they might not fight as well, but again you decide if you take it into account.
 
-        After you have made you decition you will return s short story and a JSON of following:
+        After you have made you decision you will return short story and a JSON of following:
         returned_attack_event  = {
-            "new_army_size": This will be the new friendl army size, based on your story. It can stay same, be smaaller or larger, you decide.
+            "new_army_size": This will be the new friendly army size, based on your story. It can stay same, be smaller or larger, you decide.
             "new_leader_status": You can give the leader new status typed {"status": which tells the leader status, "effect": {<effect name>: <effect>}
-            "success": ture or false depending how you decide
-            "random_resources_from_the_story": if you gave some random items in the story, they are listed here in type {"item_name":<item name>, "effect":<effect>}
+            "success": true or false depending how you decide was the attack successful or not.
+            "resources_from_story": if you gave player some items in the story, list them here in type {"item_name":<item name>, "effect": {<effect name>: effect}}
             "army_status_effect": You can also give the army some effect, just like leader status
-        }'''
+        }
+        JSON you return must be named returned_attack_event and in the end of the text.
+        The to_attack JSON is as follows:
+        '''
 
     DIPLOMATIC_JSON = '''
-        Player is tyring to make a peaceful diplomatic alliance with the country (mentioned in JSON) which if succesfull will join the counrty to players empire.
-        diplomatic_deal = {
-            "in_turn": this field will contain the players name who is makin a move
-            "where": contains the country player are tryign to target.
-            "leader_Status": leader status. will be type {"status": which tells the leader status, "effect": {effect name: effect}. Use the leader status in your story and decition making. Take in count the effect the leader has, but it's more like a guideline, so in the end you make  the desition, how much it affects the result.
-            "friendly_army_size": is numer of the players army size
+        Player is trying to make a peaceful diplomatic alliance with the country (mentioned in JSON) which if successful will merge the country to players empire.
+        diplomatic = {
+            "in_turn": this field will contain the players name who is making a move
+            "where": contains the country player are trying to target.
+            "leader_Status": leader status will be type {"status": <which tells the leader status>, "effect": {<effect name>: <effect>}. You can Use the leader status in your story and decision making, but it is optional.
+            "friendly_army_size": is number of the players army size
             "friendly_army_status": is type of {"status": which tells the army status, "effect": {effect name: effect}. Work similarly to leader_status.
             "enemy_army_size": is number of enemy army(the country player are attacking)
-            "enemy_army_status":  is type of {"status": which tells the enemy army status, "effect": {effect name: effect}. Work similarly to leader_status.
-            "enemy_leader_status":  is type of {"status": which tells the enemy leader status, "effect": {effect name: effect}. Work similarly to leader_status.
+            "enemy_army_status": is type of {"status": which tells the enemy army status, "effect": {effect name: effect}. Work similarly to leader_status.
+            "enemy_leader_status": is type of {"status": which tells the enemy leader status, "effect": {effect name: effect}. Work similarly to leader_status.
             "used_special_resources": is type {"item": <item name>, "effect": <effect>} works similarly to leader_status 
-            "offered_resource": This will be a list of resources player have offered  to "sweeten the deal". Every country had a favourite and hated resource and take them into calculations when you make the desition of the event and story. Again there are just guidelines, you deicide the result in the end.
+            "offered_resource": This will be a list of resources player have offered  to "sweeten the deal". Every country had a favorite and hated resource and take them into calculations when you make the decision of the event and story. Again there are just guidelines, you deicide the result in the end.
             "enemy_hated_resource: This will contain the resource the country hates and if player offers this to "sweeten the deal" it might have negative effect on the success, but again, you choose if it has or not.
             "enemy_loved_resource: This will contain the resource the country hates and if player offers this to "sweeten the deal" it might have positive effect on the success, but again, you choose if it has or not.
         }
-        From the JOSN data, you need to make a conclusion was the attack succesfull and give a short story of the events of the battle.
-        You and make the story as imaginative as possible. 
-        Use the army sizes, the different statused and their effects and the used_special_recources and their statuses. For example, friedly_army_size is 500 000 soldies larger than the enemy_army_size, but friendly_army_status is {"status": "Drunk", "effect": {"combat effectiveness": "decrease"}}, so take stuff like that into account, but you choose how much you will follow the data in the JSON.
+        From the JSON data, you need to make a conclusion was the diplomatic deal successful.
+        Use the army sizes, the different statuses and their effects and the used_special_recourses and their statuses. 
 
-        returned_diplomati_event = {
+        After you have made you decision you will return short story and a JSON of following:
+        returned_diplomatic_event = {
             "new_leader_status": You can give the leader new status typed {"status": which tells the leader status, "effect": {<effect name>: <effect>}
             "success": yes or no depending how you decide
             "random_resources_from_the_story": if you game some random items in the story, they are listed here in type {"itemName":<item name>, "effect":<effect>}
             "army_status_effect": You can also give the army some effect, just like leader status
         }
-        '''
-    
+        JSON you return must be named returned_diplomatic_event and in the end of the text.
+        The diplomatic JSON is as follows:        '''
+
+    text = '''
+            Returning to the JSON data, the updated attack event information would be as follows:\n\nreturned_attack_event = {\n    \"new_army_size\": 4100,\n    \"new_leader_status\": {\n        \"status\": \"Victorious Conqueror\",\n        \"effect\": {\"morale\": \"increase\"}\n    },\n    \"success\": true,\n    \"resources_from_story\": [\n        {\"item_name\": \"fish\", \"effect\": {\"combat effectiveness\": \"increase\"}},\n        {\"item_name\": \"food\", \"effect\": {\"morale\": \"increase\"}}\n    ],\n    \"army_status_effect\": null\n}\n\nWith the successful conquest of Finland, Player's army stood at a new size of 4100 soldiers. The leader, now hailed as the \"Victorious Conqueror,\" had an effect that boosted the morale of the army. The \"fish\" resource was found
+
+    '''
+    # print(text.split('returned_attack_event')[1])
+    # Extract everything between curly braces {}s
+    json_match = re.search(r'\{[^}]+\}', text)
+
+    if json_match:
+        json_data = json_match.group(0)
+
+        # Load the JSON data as a dictionary
+        try:
+            parsed_data = json.loads(json_data)
+
+            # Print the extracted JSON data
+            print(json.dumps(parsed_data, indent=2))
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON data: {e}")
+    else:
+        print("JSON data not found in the text.")
+
+    def call_openai(prompt, data):
+
+        action = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo-16k",
+            # prompt=STARTING_PROMPT + ATTACK_JSON,
+            messages=[
+                {"role": "user", "content": STARTING_PROMPT + ATTACK_JSON + data}
+            ],
+            temperature=0.8,
+            max_tokens=3500
+        )
+        print(action)
+
     def to_send(special_items):
         used_special_resources = [i for i in special_items]
         print(used_special_resources)
         # TODO: Update JSON
         data = {
             "in_turn": in_turn,
-            "action": action,
             "where": country_to_attack,
             "leader_Status": player_leader_status,
             "friendly_army_size": player_army_size,
@@ -89,7 +130,7 @@ def main_loop():
             "resources": player_resources,
             "used_special_resources": used_special_resources
         }
-        
+
         return json.dumps(data)
     # Ask player for staring country
     while True:
@@ -123,7 +164,7 @@ def main_loop():
         players_turn = not players_turn
         # Ask player for action
         action = input(
-            'Please choose an action. a: attack, d: Diplomatic alliance. q: Quits the game.  ')
+            'Please choose an action. a: attack, d: Diplomatic alliance. q: Quits the game.')
         # Exit command
         if action == 'q':
             game_active = False
@@ -133,7 +174,7 @@ def main_loop():
             print('Neighbor countries:', player_neighbors)
             # Player input
             country_to_attack = input(
-                'Which neighbor country do you want to attack? Type its name. c: cancel. ')
+                'Which neighbor country do you want to attack? Type its name. c: cancel.')
 
             # Cancel action and exit loop to main menu
             if country_to_attack == 'c':
@@ -146,42 +187,41 @@ def main_loop():
                 if country_to_attack in country['neighbor_countries']:
                     # TODO: AI event choosing here
 
-                    test = [{'item': 'fish', 'effect': 'magic'},
-                            {'item': 'food', 'effect': 'morale'}]
+                    test_items = [{'item': 'fish', 'effect': 'magic'},
+                                  {'item': 'food', 'effect': 'morale'}]
 
                     # Create json of data to send to AI
-                    json_to_send = to_send(test)
-                    print('tosend')
-                    print(json_to_send)
+                    data = to_send(test_items)
+                    print(data)
+                    call_openai(ATTACK_JSON, data)
+                    # is_attack_successful = random.randint(0, 1)
+                    # if is_attack_successful:
 
-                    is_attack_successful = random.randint(0, 1)
-                    if is_attack_successful:
+                    #     # Add the attacked country to player
+                    #     player_countries.append(eval(country_to_attack))
+                    #     player_countries_string.append(
+                    #         eval(country_to_attack)['name'])
 
-                        # Add the attacked country to player
-                        player_countries.append(eval(country_to_attack))
-                        player_countries_string.append(
-                            eval(country_to_attack)['name'])
+                    #     # Update the border
+                    #     player_neighbors.update(
+                    #         eval(country_to_attack)['neighbor_countries'])
+                    #     player_neighbors = player_neighbors - \
+                    #         set(player_countries_string)
 
-                        # Update the border
-                        player_neighbors.update(
-                            eval(country_to_attack)['neighbor_countries'])
-                        player_neighbors = player_neighbors - \
-                            set(player_countries_string)
+                    #     print('You successfully conquered', country_to_attack,
+                    #           '. Yor empire now consists of ', player_countries_string)
 
-                        print('You successfully conquered', country_to_attack,
-                              '. Yor empire now consists of ', player_countries_string)
+                    #     # Choose randomly new leader_status and update it.
+                    #     player_leader_status = random.choice(
+                    #         leader_effects)
 
-                        # Choose randomly new leader_status and update it.
-                        player_leader_status = random.choice(
-                            leader_effects)
-
-                        print('While attacking your leader status changed to',
-                              player_start_country['leader_status'])
-                        print('Your new neighbors countries are',
-                              player_neighbors)
-                        break
-                    else:
-                        print('attack failed')
+                    #     print('While attacking your leader status changed to',
+                    #           player_start_country['leader_status'])
+                    #     print('Your new neighbors countries are',
+                    #           player_neighbors)
+                    #     break
+                    # else:
+                    #     print('attack failed')
                 else:
                     print("I'm sorry, couldn't find that country")
                     break
